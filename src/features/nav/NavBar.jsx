913670177facee472781ed30d1910c6e760d8007
Elvan Hidayat <elvanhd@gmail.com>
2023-03-30 00:59:16 +0700

    add menus to the navbar

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,31 +1,36 @@
+import { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
+import SignedInMenu from './SignedInMenu';
+import SignedOutMenu from './SignedOutMenu';
 
-export default function NavBar({ setFormOpen }) {
+export default function NavBar() {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  function handleSignOut() {
+    setAuthenticated(false);
+    history.push('/');
+  }
+
   return (
     <Menu inverted fixed='top'>
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} exact to='/' header>
           <img src='/assets/logo.png' alt='logo' style={{ marginRight: 15 }} />
-          SocialEvents
+          Re-vents
         </Menu.Item>
-        <Menu.Item name='Events' />
-        <Menu.Item>
-          <Button
-            onClick={() => setFormOpen(true)}
-            positive
-            inverted
-            content='Create Event'
-          />
-        </Menu.Item>
-        <Menu.Item position='right'>
-          <Button basic inverted content='Login' />
-          <Button
-            basic
-            inverted
-            content='Register'
-            style={{ marginLeft: '0.5em' }}
-          />
-        </Menu.Item>
+        <Menu.Item as={NavLink} to='/events' name='Events' />
+        {authenticated && (
+          <Menu.Item as={NavLink} to='/createEvent'>
+            <Button positive inverted content='Create Event' />
+          </Menu.Item>
+        )}
+        {authenticated ? (
+          <SignedInMenu signOut={handleSignOut} />
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated} />
+        )}
       </Container>
     </Menu>
   );
---
+++ src/features/nav/SignedOutMenu.jsx
@@ -0,0 +1,20 @@
+import { Button, Menu } from 'semantic-ui-react';
+
+export default function SignedOutMenu({ setAuthenticated }) {
+  return (
+    <Menu.Item position='right'>
+      <Button
+        onClick={() => setAuthenticated(true)}
+        basic
+        inverted
+        content='Login'
+      />
+      <Button
+        basic
+        inverted
+        content='Register'
+        style={{ marginLeft: '0.5em' }}
+      />
+    </Menu.Item>
+  );
+}
