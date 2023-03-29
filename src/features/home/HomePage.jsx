72199da470f87b439c78823a6b930fbdc050f5de
Elvan Hidayat <elvanhd@gmail.com>
2023-03-30 00:34:26 +0700

    create the home page and add its styling

---
+++ src/features/home/HomePage.jsx
@@ -0,0 +1,29 @@
+import {
+  Button,
+  Container,
+  Header,
+  Icon,
+  Image,
+  Segment,
+} from 'semantic-ui-react';
+
+export default function HomePage({ history }) {
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container>
+        <Header as='h1' inverted>
+          <Image
+            size='massive'
+            src='/assets/logo.png'
+            style={{ marginBottom: 12 }}
+          />
+          SocialEvents
+        </Header>
+        <Button onClick={() => history.push('/events')} size='huge' inverted>
+          Get started
+          <Icon name='right arrow' inverted />
+        </Button>
+      </Container>
+    </Segment>
+  );
+}
