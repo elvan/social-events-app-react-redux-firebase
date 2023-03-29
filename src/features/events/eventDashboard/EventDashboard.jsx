e7286bcf860f08fb12bd49c0a7014ef8b71384ee
Elvan Hidayat <elvanhd@gmail.com>
2023-03-29 22:49:31 +0700

    create an event-form and manage component state

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,14 +1,17 @@
 import { Grid } from 'semantic-ui-react';
 import { sampleData } from '../../../app/api/sampleData';
+import EventForm from '../eventForm/EventForm';
 import EventList from './EventList';
 
-export default function EventDashboard() {
+export default function EventDashboard({ formOpen, setFormOpen }) {
   return (
     <Grid>
       <Grid.Column width={10}>
         <EventList events={sampleData} />
       </Grid.Column>
-      <Grid.Column width={6}>EventForm</Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+      </Grid.Column>
     </Grid>
   );
 }
