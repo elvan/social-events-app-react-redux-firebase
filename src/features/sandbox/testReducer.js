a137a9e39832311ecac207dec9c77b5b9e54f369
Elvan Hidayat <elvanhd@gmail.com>
2023-03-30 03:27:52 +0700

    redux action creators

--- src/features/sandbox/testReducer.js
+++ src/features/sandbox/testReducer.js
@@ -1,6 +1,20 @@
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 
+export function increment(amount) {
+  return {
+    type: INCREMENT_COUNTER,
+    payload: amount,
+  };
+}
+
+export function decrement(amount) {
+  return {
+    type: DECREMENT_COUNTER,
+    payload: amount,
+  };
+}
+
 const initialState = {
   data: 42,
 };
