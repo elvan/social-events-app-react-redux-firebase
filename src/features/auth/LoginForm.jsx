8a0db2f38d2052f1a4e495cdb620c22b5114c4a3
Elvan Hidayat <elvanhd@gmail.com>
2023-03-30 22:41:39 +0700

    create the login form

---
+++ src/features/auth/LoginForm.jsx
@@ -0,0 +1,49 @@
+import { Form, Formik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { Button } from 'semantic-ui-react';
+import * as Yup from 'yup';
+import MyTextInput from '../../app/common/form/MyTextInput';
+import { closeModal } from '../../app/common/modals/modalReducer';
+import ModalWrapper from '../../app/common/modals/ModalWrapper';
+import { signInUser } from './authActions';
+
+export default function LoginForm() {
+  const dispatch = useDispatch();
+
+  return (
+    <ModalWrapper size='mini' header='Sign in to SocialEvents'>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={Yup.object({
+          email: Yup.string().required().email(),
+          password: Yup.string().required(),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          dispatch(signInUser(values));
+          setSubmitting(false);
+          dispatch(closeModal());
+        }}
+      >
+        {({ isSubmitting, isValid, dirty }) => (
+          <Form className='ui form'>
+            <MyTextInput name='email' placeholder='Email Address' />
+            <MyTextInput
+              name='password'
+              placeholder='Password'
+              type='password'
+            />
+            <Button
+              loading={isSubmitting}
+              disabled={!isValid || !dirty || isSubmitting}
+              type='submit'
+              fluid
+              size='large'
+              color='teal'
+              content='Login'
+            />
+          </Form>
+        )}
+      </Formik>
+    </ModalWrapper>
+  );
+}
