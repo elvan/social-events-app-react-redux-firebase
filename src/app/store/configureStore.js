152597b8d84ca662ddb1e8df224028f1873870ea
Elvan Hidayat <elvanhd@gmail.com>
2023-04-02 03:14:53 +0700

    persisting the login

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,8 +1,16 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { verifyAuth } from '../../features/auth/authActions';
 import rootReducer from './rootReducer';
 
 export function configureStore() {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+
+  store.dispatch(verifyAuth());
+
+  return store;
 }
--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -1,9 +1,25 @@
+import { APP_LOADED } from '../../app/async/asyncReducer';
+import firebase from '../../app/config/firebase';
 import { SIGN_IN_USER, SIGN_OUT_USER } from './authConstants';
 
 export function signInUser(user) {
   return {
     type: SIGN_IN_USER,
     payload: user,
+  };
+}
+
+export function verifyAuth() {
+  return function (dispatch) {
+    return firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(signInUser(user));
+        dispatch({ type: APP_LOADED });
+      } else {
+        dispatch(signOutUser());
+        dispatch({ type: APP_LOADED });
+      }
+    });
   };
 }
 
