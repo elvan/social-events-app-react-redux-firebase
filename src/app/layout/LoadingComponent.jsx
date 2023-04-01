42a8d16a2948077f9b6de5db9dc8e9433354c87b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-01 23:00:14 +0700

    add a loading component

---
+++ src/app/layout/LoadingComponent.jsx
@@ -0,0 +1,12 @@
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+export default function LoadingComponent({
+  inverted = true,
+  content = 'Loading...',
+}) {
+  return (
+    <Dimmer inverted={inverted} active={true}>
+      <Loader content={content} />
+    </Dimmer>
+  );
+}
