d13b427692ea9efe1c709b6869997de28efbb8be
Elvan Hidayat <elvanhd@gmail.com>
2023-04-01 22:48:04 +0700

    add isolate the loading indicators

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -8,7 +8,9 @@ import TestPlaceInput from './TestPlaceInput';
 
 export default function Sandbox() {
   const dispatch = useDispatch();
+  const [target, setTarget] = useState(null);
   const data = useSelector((state) => state.test.data);
+  const { loading } = useSelector((state) => state.async);
   const defaultProps = {
     center: {
       lat: -6.912,
@@ -27,12 +29,22 @@ export default function Sandbox() {
       <h1>Testing 123</h1>
       <h3>The data is: {data}</h3>
       <Button
-        onClick={() => dispatch(increment(20))}
+        name='increment'
+        loading={loading && target === 'increment'}
+        onClick={(e) => {
+          dispatch(increment(20));
+          setTarget(e.target.name);
+        }}
         content='Increment'
         color='green'
       />
       <Button
-        onClick={() => dispatch(decrement(10))}
+        name='decrement'
+        loading={loading && target === 'decrement'}
+        onClick={(e) => {
+          dispatch(decrement(10));
+          setTarget(e.target.name);
+        }}
         content='Decrement'
         color='red'
       />
