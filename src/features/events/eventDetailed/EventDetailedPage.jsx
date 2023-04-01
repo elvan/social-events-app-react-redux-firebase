33dbb4b93c8d6595d97fbc9cecef3964162d60ae
Elvan Hidayat <elvanhd@gmail.com>
2023-04-02 01:43:31 +0700

    fetch a single event data from firestore document

--- src/features/events/eventDetailed/EventDetailedPage.jsx
+++ src/features/events/eventDetailed/EventDetailedPage.jsx
@@ -1,14 +1,33 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
+import { listenToEventFromFirestore } from '../../../app/firestore/firestoreService';
+import useFirestoreDoc from '../../../app/hooks/useFirestoreDoc';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { listenToEvents } from '../eventActions';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedHeader from './EventDetailedHeader';
 import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedSidebar from './EventDetailedSidebar';
 
 export default function EventDetailedPage({ match }) {
+  const dispatch = useDispatch();
   const event = useSelector((state) =>
     state.event.events.find((e) => e.id === match.params.id)
   );
+  const { loading, error } = useSelector((state) => state.async);
+
+  useFirestoreDoc({
+    query: () => listenToEventFromFirestore(match.params.id),
+    data: (event) => dispatch(listenToEvents([event])),
+    deps: [match.params.id, dispatch],
+  });
+
+  if (loading || (!event && !error))
+    return <LoadingComponent content='Loading event...' />;
+
+  if (error) return <Redirect to='/error' />;
+
   return (
     <Grid>
       <Grid.Column width={10}>
@@ -17,7 +36,7 @@ export default function EventDetailedPage({ match }) {
         <EventDetailedChat />
       </Grid.Column>
       <Grid.Column width={6}>
-        <EventDetailedSidebar attendees={event.attendees} />
+        <EventDetailedSidebar attendees={event?.attendees} />
       </Grid.Column>
     </Grid>
   );
