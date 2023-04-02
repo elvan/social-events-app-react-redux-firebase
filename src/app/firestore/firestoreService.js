2730ed5d896560bb197b89cffc686ce1ff64bf8f
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 02:18:21 +0700

    adding a followUser firestore function

--- src/app/firestore/firestoreService.js
+++ src/app/firestore/firestoreService.js
@@ -202,3 +202,28 @@ export function getUserEventsQuery(activeTab, userUid) {
         .orderBy('date');
   }
 }
+
+export async function followUser(profile) {
+  const user = firebase.auth().currentUser;
+  const batch = db.batch();
+  try {
+    batch.set(
+      db
+        .collection('following')
+        .doc(user.uid)
+        .collection('userFollowing')
+        .doc(profile.id),
+      {
+        displayName: profile.displayName,
+        photoURL: profile.photoURL,
+        uid: profile.id,
+      }
+    );
+    batch.update(db.collection('users').doc(user.uid), {
+      followingCount: firebase.firestore.FieldValue.increment(1),
+    });
+    return await batch.commit();
+  } catch (error) {
+    throw error;
+  }
+}
