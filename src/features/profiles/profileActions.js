980e5ab859e4a04f61173f259e025ef9c22b62d0
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 02:47:05 +0700

    adding follow and unfollow redux actions and reducers

--- src/features/profiles/profileActions.js
+++ src/features/profiles/profileActions.js
@@ -5,6 +5,8 @@ import {
   LISTEN_TO_SELECTED_USER_PROFILE,
   LISTEN_TO_USER_EVENTS,
   LISTEN_TO_USER_PHOTOS,
+  SET_FOLLOW_USER,
+  SET_UNFOLLOW_USER,
 } from './profileConstants';
 
 export function listenToCurrentUserProfile(profile) {
@@ -46,5 +48,17 @@ export function listenToFollowings(followings) {
   return {
     type: LISTEN_TO_FOLLOWINGS,
     payload: followings,
+  };
+}
+
+export function setFollowUser() {
+  return {
+    type: SET_FOLLOW_USER,
+  };
+}
+
+export function setUnfollowUser() {
+  return {
+    type: SET_UNFOLLOW_USER,
   };
 }
--- src/features/profiles/profileReducer.js
+++ src/features/profiles/profileReducer.js
@@ -1,10 +1,13 @@
 import {
+  CLEAR_FOLLOWINGS,
   LISTEN_TO_CURRENT_USER_PROFILE,
   LISTEN_TO_FOLLOWERS,
   LISTEN_TO_FOLLOWINGS,
   LISTEN_TO_SELECTED_USER_PROFILE,
   LISTEN_TO_USER_EVENTS,
   LISTEN_TO_USER_PHOTOS,
+  SET_FOLLOW_USER,
+  SET_UNFOLLOW_USER,
 } from './profileConstants';
 
 const initialState = {
@@ -14,6 +17,7 @@ const initialState = {
   profileEvents: [],
   followers: [],
   followings: [],
+  followingUser: false,
 };
 
 export default function profileReducer(
@@ -50,6 +54,22 @@ export default function profileReducer(
       return {
         ...state,
         followings: payload,
+      };
+    case SET_FOLLOW_USER:
+      return {
+        ...state,
+        followingUser: true,
+      };
+    case SET_UNFOLLOW_USER:
+      return {
+        ...state,
+        followingUser: false,
+      };
+    case CLEAR_FOLLOWINGS:
+      return {
+        ...state,
+        followers: [],
+        followings: [],
       };
     default: {
       return state;
