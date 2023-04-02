fe7b6805ddb2cd1de2c1d23cda24d87c82ca1b1e
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 00:55:24 +0700

    setting up firebase

--- src/app/firestore/firebaseService.js
+++ src/app/firestore/firebaseService.js
@@ -62,3 +62,20 @@ export function deleteFromFirebaseStorage(filename) {
   const photoRef = storageRef.child(`${userUid}/user_images/${filename}`);
   return photoRef.delete();
 }
+
+export function addEventChatComment(eventId, values) {
+  const user = firebase.auth().currentUser;
+  const newComment = {
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+    uid: user.uid,
+    text: values.comment,
+    date: Date.now(),
+    parentId: values.parentId,
+  };
+  return firebase.database().ref(`chat/${eventId}`).push(newComment);
+}
+
+export function getEventChatRef(eventId) {
+  return firebase.database().ref(`chat/${eventId}`).orderByKey();
+}
