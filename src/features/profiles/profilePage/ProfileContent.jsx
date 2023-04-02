828ce7b0fdc17bb2b69e823fbd576d2ecb3da86a
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 04:11:40 +0700

    handle follow and unfollow users

--- src/features/profiles/profilePage/ProfileContent.jsx
+++ src/features/profiles/profilePage/ProfileContent.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab } from 'semantic-ui-react';
 import AboutTab from './AboutTab';
 import EventsTab from './EventsTab';
+import FollowingTab from './FollowingTab';
 import PhotosTab from './PhotosTab';
 
 export default function ProfileContent({ profile, isCurrentUser }) {
+  const [activeTab, setActiveTab] = useState(0);
   const panes = [
     {
       menuItem: 'About',
@@ -19,8 +21,26 @@ export default function ProfileContent({ profile, isCurrentUser }) {
       ),
     },
     { menuItem: 'Events', render: () => <EventsTab profile={profile} /> },
-    { menuItem: 'Followers', render: () => <Tab.Pane>Followers</Tab.Pane> },
-    { menuItem: 'Following', render: () => <Tab.Pane>Following</Tab.Pane> },
+    {
+      menuItem: 'Followers',
+      render: () => (
+        <FollowingTab
+          key={profile.id}
+          profile={profile}
+          activeTab={activeTab}
+        />
+      ),
+    },
+    {
+      menuItem: 'Following',
+      render: () => (
+        <FollowingTab
+          key={profile.id}
+          profile={profile}
+          activeTab={activeTab}
+        />
+      ),
+    },
   ];
 
   return (
@@ -28,6 +48,7 @@ export default function ProfileContent({ profile, isCurrentUser }) {
       menu={{ fluid: true, vertical: true }}
       menuPosition='right'
       panes={panes}
+      onTabChange={(e, data) => setActiveTab(data.activeIndex)}
     />
   );
 }
--- src/features/profiles/profilePage/ProfileHeader.jsx
+++ src/features/profiles/profilePage/ProfileHeader.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import {
   Button,
   Divider,
@@ -10,10 +11,61 @@ import {
   Segment,
   Statistic,
 } from 'semantic-ui-react';
+import {
+  followUser,
+  getFollowingDoc,
+  unfollowUser,
+} from '../../../app/firestore/firestoreService';
+import { setFollowUser, setUnfollowUser } from '../profileActions';
+import { CLEAR_FOLLOWINGS } from '../profileConstants';
 
 export default function ProfileHeader({ profile, isCurrentUser }) {
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const { followingUser } = useSelector((state) => state.profile);
+
+  useEffect(() => {
+    if (isCurrentUser) return;
+    setLoading(true);
+    async function fetchFollowingDoc() {
+      try {
+        const followingDoc = await getFollowingDoc(profile.id);
+        if (followingDoc && followingDoc.exists) {
+          dispatch(setFollowUser());
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
+    fetchFollowingDoc().then(() => setLoading(false));
+    return () => {
+      dispatch({ type: CLEAR_FOLLOWINGS });
+    };
+  }, [dispatch, profile.id, isCurrentUser]);
+
+  async function handleFollowUser() {
+    setLoading(true);
+    try {
+      await followUser(profile);
+      dispatch(setFollowUser());
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleUnfollowUser() {
+    setLoading(true);
+    try {
+      await unfollowUser(profile);
+      dispatch(setUnfollowUser());
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <Segment>
@@ -54,6 +106,11 @@ export default function ProfileHeader({ profile, isCurrentUser }) {
                 </Reveal.Content>
                 <Reveal.Content hidden style={{ width: '100%' }}>
                   <Button
+                    onClick={
+                      followingUser
+                        ? () => handleUnfollowUser()
+                        : () => handleFollowUser()
+                    }
                     loading={loading}
                     basic
                     fluid
