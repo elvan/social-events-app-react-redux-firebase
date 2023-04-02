6fb2b3bac52e7d4198ee7018dbf572a41d387cfd
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 01:05:57 +0700

    add listen to chats reducers and actions

--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -8,6 +8,7 @@ import {
   CREATE_EVENT,
   DELETE_EVENT,
   FETCH_EVENTS,
+  LISTEN_TO_EVENT_CHAT,
   UPDATE_EVENT,
 } from './eventConstants';
 
@@ -49,5 +50,12 @@ export function deleteEvent(eventId) {
   return {
     type: DELETE_EVENT,
     payload: eventId,
+  };
+}
+
+export function listenToEventChat(comments) {
+  return {
+    type: LISTEN_TO_EVENT_CHAT,
+    payload: comments,
   };
 }
