4e5a14994d5b915edc73887795e13162b6253944
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 01:24:50 +0700

    listening and displaying chat data comments

--- src/features/events/eventDetailed/EventDetailedChat.jsx
+++ src/features/events/eventDetailed/EventDetailedChat.jsx
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
-import { Header, Segment } from 'semantic-ui-react';
+import { formatDistance } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Comment, Header, Segment } from 'semantic-ui-react';
+import { createDataTree } from '../../../app/common/util/util';
+import {
+  firebaseObjectToArray,
+  getEventChatRef,
+} from '../../../app/firestore/firebaseService';
+import { listenToEventChat } from '../eventActions';
+import { CLEAR_COMMENTS } from '../eventConstants';
 import EventDetailedChatForm from './EventDetailedChatForm';
 
 export default function EventDetailedChat({ eventId }) {
+  const dispatch = useDispatch();
+  const { comments } = useSelector((state) => state.event);
   const [showReplyForm, setShowReplyForm] = useState({
     open: false,
     commentId: null,
   });
+
+  useEffect(() => {
+    getEventChatRef(eventId).on('value', (snapshot) => {
+      if (!snapshot.exists()) return;
+      dispatch(
+        listenToEventChat(firebaseObjectToArray(snapshot.val()).reverse())
+      );
+    });
+    return () => {
+      dispatch({ type: CLEAR_COMMENTS });
+      getEventChatRef().off();
+    };
+  }, [eventId, dispatch]);
 
   return (
     <>
@@ -26,6 +51,29 @@ export default function EventDetailedChat({ eventId }) {
           parentId={0}
           closeForm={setShowReplyForm}
         />
+        <Comment.Group>
+          {createDataTree(comments).map((comment) => (
+            <Comment key={comment.id}>
+              <Comment.Avatar src={comment.photoURL || '/assets/user.png'} />
+              <Comment.Content>
+                <Comment.Author as={Link} to={`/profile/${comment.uid}`}>
+                  {comment.displayName}
+                </Comment.Author>
+                <Comment.Metadata>
+                  <div>{formatDistance(comment.date, new Date())}</div>
+                </Comment.Metadata>
+                <Comment.Text>
+                  {comment.text.split('\n').map((text, i) => (
+                    <span key={i}>
+                      {text}
+                      <br />
+                    </span>
+                  ))}
+                </Comment.Text>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </Comment.Group>
       </Segment>
     </>
   );
