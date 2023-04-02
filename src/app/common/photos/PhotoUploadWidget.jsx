b30aa20ad3bac34b5de49f19cbf101eb538905c6
Elvan Hidayat <elvanhd@gmail.com>
2023-04-02 17:44:08 +0700

    add a profile photos page

---
+++ src/app/common/photos/PhotoUploadWidget.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Button, Grid, Header } from 'semantic-ui-react';
+
+export default function PhotoUploadWidget({ setEditMode }) {
+  const [files, setFiles] = useState([]);
+
+  return (
+    <Grid>
+      <Grid.Column width={4}>
+        <Header color='teal' sub content='Step 1 - Add Photo' />
+        PhotoWidgetDropzone
+      </Grid.Column>
+      <Grid.Column width={1} />
+      <Grid.Column width={4}>
+        <Header color='teal' sub content='Step 2 - Resize' />
+        PhotoWidgetCropper
+      </Grid.Column>
+      <Grid.Column width={1} />
+      <Grid.Column width={4}>
+        <Header color='teal' sub content='Step 3 - Preview & upload' />
+        {files.length > 0 && (
+          <>
+            <div
+              className='img-preview'
+              style={{ minHeight: 200, minWidth: 200, overflow: 'hidden' }}
+            />
+            <Button.Group>
+              <Button style={{ width: 100 }} positive icon='check' />
+              <Button style={{ width: 100 }} icon='close' />
+            </Button.Group>
+          </>
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+}
---
+++ src/features/profiles/profilePage/PhotosTab.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, Card, Grid, Header, Image, Tab } from 'semantic-ui-react';
+import PhotoUploadWidget from '../../../app/common/photos/PhotoUploadWidget';
+
+export default function PhotosTab({ profile, isCurrentUser }) {
+  const dispatch = useDispatch();
+  const [editMode, setEditMode] = useState(false);
+  const { loading } = useSelector((state) => state.async);
+  const { photos } = useSelector((state) => state.profile);
+  const [updating, setUpdating] = useState({ isUpdating: false, target: null });
+  const [deleting, setDeleting] = useState({ isDeleting: false, target: null });
+
+  return (
+    <Tab.Pane loading={loading}>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated='left' icon='user' content={`Photos`} />
+          {isCurrentUser && (
+            <Button
+              onClick={() => setEditMode(!editMode)}
+              floated='right'
+              basic
+              content={editMode ? 'Cancel' : 'Add Photo'}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {editMode ? (
+            <PhotoUploadWidget setEditMode={setEditMode} />
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {photos.map((photo) => (
+                <Card key={photo.id}>
+                  <Image src={photo.url} />
+                  <Button.Group fluid widths={2}>
+                    <Button
+                      name={photo.id}
+                      loading={
+                        updating.isUpdating && updating.target === photo.id
+                      }
+                      disabled={photo.url === profile.photoURL}
+                      basic
+                      color='green'
+                      content='Main'
+                    />
+                    <Button
+                      name={photo.id}
+                      loading={
+                        deleting.isDeleting && deleting.target === photo.id
+                      }
+                      disabled={photo.url === profile.photoURL}
+                      basic
+                      color='red'
+                      icon='trash'
+                    />
+                  </Button.Group>
+                </Card>
+              ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+}
