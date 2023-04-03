3623bf133372914976c891f4b0cd67c682456cd4
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 15:52:07 +0700

    creating an addFollowing cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+exports.addFollowing = functions.firestore
+  .document('following/{userUid}/userFollowing/{profileId}')
+  .onCreate(async (snapshot, context) => {
+    const following = snapshot.data();
+    console.log({ following });
+    try {
+      const userDoc = await db
+        .collection('users')
+        .doc(context.params.userUid)
+        .get();
+      const batch = db.batch();
+      batch.set(
+        db
+          .collection('following')
+          .doc(context.params.profileId)
+          .collection('userFollowers')
+          .doc(context.params.userUid),
+        {
+          displayName: userDoc?.data()?.displayName,
+          photoURL: userDoc?.data()?.photoURL,
+          uid: userDoc.id,
+        }
+      );
+      batch.update(db.collection('users').doc(context.params.profileId), {
+        followerCount: admin.firestore.FieldValue.increment(1),
+      });
+      return await batch.commit();
+    } catch (error) {
+      return console.log(error);
+    }
+  });
