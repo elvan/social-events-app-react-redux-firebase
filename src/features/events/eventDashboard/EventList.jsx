faea89f3e01795530dc47f28a95988857804372f
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 18:44:57 +0700

    adding infinite scroll

--- src/features/events/eventDashboard/EventList.jsx
+++ src/features/events/eventDashboard/EventList.jsx
@@ -1,11 +1,27 @@
+import React from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 import EventListItem from './EventListItem';
 
-export default function EventList({ events }) {
+export default function EventList({
+  events,
+  getNextEvents,
+  loading,
+  moreEvents,
+}) {
   return (
     <>
-      {events.map((event) => (
-        <EventListItem event={event} key={event.id} />
-      ))}
+      {events.length !== 0 && (
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={getNextEvents}
+          hasMore={!loading && moreEvents}
+          initialLoad={false}
+        >
+          {events.map((event) => (
+            <EventListItem event={event} key={event.id} />
+          ))}
+        </InfiniteScroll>
+      )}
     </>
   );
 }
