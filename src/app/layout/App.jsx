8101ddf95f3835e2e622d3db04332b90b939978a
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 22:14:41 +0700

    creating a private route

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -14,6 +14,7 @@ import Sandbox from '../../features/sandbox/Sandbox';
 import ErrorComponent from '../common/errors/ErrorComponent';
 import ModalManager from '../common/modals/ModalManager';
 import LoadingComponent from './LoadingComponent';
+import PrivateRoute from './PrivateRoute';
 
 export default function App() {
   const { key } = useLocation();
@@ -35,13 +36,13 @@ export default function App() {
               <Route exact path='/events' component={EventDashboard} />
               <Route exact path='/sandbox' component={Sandbox} />
               <Route path='/events/:id' component={EventDetailedPage} />
-              <Route
+              <PrivateRoute
                 path={['/createEvent', '/manage/:id']}
                 component={EventForm}
                 key={key}
               />
-              <Route path='/account' component={AccountPage} />
-              <Route path='/profile/:id' component={ProfilePage} />
+              <PrivateRoute path='/account' component={AccountPage} />
+              <PrivateRoute path='/profile/:id' component={ProfilePage} />
               <Route path='/error' component={ErrorComponent} />
             </Container>
           </>
