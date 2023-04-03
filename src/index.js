b19a95f52879bfcfc661eef6f864761f415f60bf
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 23:40:47 +0700

    using connected router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+import { ConnectedRouter } from 'connected-react-router';
+import React from 'react';
 import 'react-calendar/dist/Calendar.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import ScrollToTop from './app/layout/ScrollToTop';
 import './app/layout/styles.css';
-import { configureStore } from './app/store/configureStore';
+import { configureStore, history } from './app/store/configureStore';
 
 const store = configureStore();
 
@@ -16,10 +17,10 @@ const rootEl = document.getElementById('root');
 function render() {
   ReactDOM.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <ConnectedRouter history={history}>
         <ScrollToTop />
         <App />
-      </BrowserRouter>
+      </ConnectedRouter>
     </Provider>,
     rootEl
   );
