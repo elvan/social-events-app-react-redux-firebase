1278e2e551ed0e35ecde347e48c666d1ff2c51fe
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 16:02:02 +0700

    adding a removeFollowing function

--- functions/index.js
+++ functions/index.js
@@ -36,3 +36,24 @@ exports.addFollowing = functions.firestore
       return console.log(error);
     }
   });
+
+exports.removeFollowing = functions.firestore
+  .document('following/{userUid}/userFollowing/{profileId}')
+  .onDelete(async (snapshot, context) => {
+    const batch = db.batch();
+    batch.delete(
+      db
+        .collection('following')
+        .doc(context.params.profileId)
+        .collection('userFollowers')
+        .doc(context.params.userUid)
+    );
+    batch.update(db.collection('users').doc(context.params.profileId), {
+      followerCount: admin.firestore.FieldValue.increment(-1),
+    });
+    try {
+      return await batch.commit();
+    } catch (error) {
+      return console.log(error);
+    }
+  });
