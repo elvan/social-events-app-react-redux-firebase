3c956c90c3b85813473464ee8f683cf8647856a3
Elvan Hidayat <elvanhd@gmail.com>
2023-04-03 22:29:16 +0700

    connecting the router to the store

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,12 +1,15 @@
+import { createBrowserHistory } from 'history';
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { verifyAuth } from '../../features/auth/authActions';
 import rootReducer from './rootReducer';
 
+export const history = createBrowserHistory();
+
 export function configureStore() {
   const store = createStore(
-    rootReducer,
+    rootReducer(history),
     composeWithDevTools(applyMiddleware(thunk))
   );
 
--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,3 +1,4 @@
+import { connectRouter } from 'connected-react-router';
 import { combineReducers } from 'redux';
 import authReducer from '../../features/auth/authReducer';
 import eventReducer from '../../features/events/eventReducer';
@@ -6,13 +7,15 @@ import testReducer from '../../features/sandbox/testReducer';
 import asyncReducer from '../async/asyncReducer';
 import modalReducer from '../common/modals/modalReducer';
 
-const rootReducer = combineReducers({
-  test: testReducer,
-  event: eventReducer,
-  modals: modalReducer,
-  auth: authReducer,
-  async: asyncReducer,
-  profile: profileReducer,
-});
+const rootReducer = (history) =>
+  combineReducers({
+    router: connectRouter(history),
+    test: testReducer,
+    event: eventReducer,
+    modals: modalReducer,
+    auth: authReducer,
+    async: asyncReducer,
+    profile: profileReducer,
+  });
 
 export default rootReducer;
