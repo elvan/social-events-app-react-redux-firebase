79cd0ba7218666caf6417581b88efed4228498ef
Elvan Hidayat <elvanhd@gmail.com>
2024-02-16 15:37:28 +0700

    Add event management functionality
    - Integrate @paralleldrive/cuid2 for unique event IDs
    - Expand AppEvent type to include 'category'
    - Implement state and handlers in App component for event selection
    - Update EventDashboard to handle event addition, update, and deletion
    - Modify EventList and EventListItem to support event operations
    - Adjust EventForm for creating and updating events with input handling
    - Utilize Semantic UI components for form and list presentation
    - Ensure event details are editable and can be updated or deleted

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,16 +1,33 @@
 import { useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/dashboard/EventDashboard';
+import { AppEvent } from '../types/event';
 import NavBar from './nav/NavBar';
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState<AppEvent | null>(null);
+
+  function handleSelectEvent(event: AppEvent | null) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
 
   return (
     <>
-      <NavBar setFormOpen={setFormOpen} />
+      <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className='main'>
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+        <EventDashboard
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          selectedEvent={selectedEvent}
+          selectEvent={handleSelectEvent}
+        />
       </Container>
     </>
   );
--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -1,31 +1,96 @@
+import { createId } from '@paralleldrive/cuid2';
+import { ChangeEvent, useState } from 'react';
 import { Button, Form, Header, Segment } from 'semantic-ui-react';
+import { AppEvent } from '../../../app/types/event';
 
 type Props = {
   setFormOpen: (value: boolean) => void;
+  addEvent: (event: AppEvent) => void;
+  selectedEvent: AppEvent | null;
+  updateEvent: (event: AppEvent) => void;
 };
 
-export default function EventForm({ setFormOpen }: Props) {
+export default function EventForm({ setFormOpen, addEvent, selectedEvent, updateEvent }: Props) {
+  const initialValues = selectedEvent ?? {
+    title: '',
+    category: '',
+    description: '',
+    city: '',
+    venue: '',
+    date: '',
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  function onSubmit() {
+    selectedEvent
+      ? updateEvent({ ...selectedEvent, ...values })
+      : addEvent({ ...values, id: createId(), hostedBy: 'bob', attendees: [], hostPhotoURL: '' });
+    setFormOpen(false);
+  }
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  }
+
   return (
     <Segment clearing>
-      <Header content='Create Event' />
-      <Form>
+      <Header content={selectedEvent ? 'Update event' : 'Create Event'} />
+      <Form onSubmit={onSubmit}>
         <Form.Field>
-          <input type='text' placeholder='Event title' />
+          <input
+            type='text'
+            placeholder='Event title'
+            value={values.title}
+            name='title'
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Category' />
+          <input
+            type='text'
+            placeholder='Category'
+            value={values.category}
+            name='category'
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Description' />
+          <input
+            type='text'
+            placeholder='Description'
+            value={values.description}
+            name='description'
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='City' />
+          <input
+            type='text'
+            placeholder='City'
+            value={values.city}
+            name='city'
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Venue' />
+          <input
+            type='text'
+            placeholder='Venue'
+            value={values.venue}
+            name='venue'
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Date' />
+          <input
+            type='date'
+            placeholder='Date'
+            value={values.date}
+            name='date'
+            onChange={(e) => handleInputChange(e)}
+          />
         </Form.Field>
 
         <Button type='submit' floated='right' positive content='Submit' />
